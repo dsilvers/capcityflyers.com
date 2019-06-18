b2ac6cf3483deee51727b3062e615ffc52803ce6
Dan Silvers <dan@silvers.net>
2019-06-18 12:58:53 -0500

    Sitemaps config

--- capcityflyers/urls.py
+++ capcityflyers/urls.py
@@ -4,19 +4,22 @@ from django.contrib import admin
 from django.views.generic.base import TemplateView
 
 from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.contrib.sitemaps.views import sitemap
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
 urlpatterns = [
     url(r'^django-admin/', admin.site.urls),
-
     url(r'^admin/', include(wagtailadmin_urls)),
+
     url(r'^documents/', include(wagtaildocs_urls)),
 
     url(r'^robots\.txt', TemplateView.as_view(
         template_name='robots.txt',
         content_type='text/plain'
     )),
+
+    url('^sitemap\.xml$', sitemap),
 
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
